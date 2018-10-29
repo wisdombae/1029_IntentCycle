c069c6f3eba09b010a2f0637ccc0807222712cf6
wisdombae <l-o-v-e-1993@nate.com>
2018-10-29 15:39:38 +0900

    Main1, Main2, Main3 XML success (& text change) & Main1.java success

--- app/src/main/java/com/bjh/www/a1029_intentcycle/MainActivity.java
+++ app/src/main/java/com/bjh/www/a1029_intentcycle/MainActivity.java
@@ -1,13 +1,25 @@
 package com.bjh.www.a1029_intentcycle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button1);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
