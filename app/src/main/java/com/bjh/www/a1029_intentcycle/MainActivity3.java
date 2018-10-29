388c4bd6c97438b1e62897e5b5a918c4d92591d9
wisdombae <l-o-v-e-1993@nate.com>
2018-10-29 15:45:46 +0900

    All success

--- app/src/main/java/com/bjh/www/a1029_intentcycle/MainActivity3.java
+++ app/src/main/java/com/bjh/www/a1029_intentcycle/MainActivity3.java
@@ -1,13 +1,25 @@
 package com.bjh.www.a1029_intentcycle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity3 extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        Button button = (Button)findViewById(R.id.button3);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
